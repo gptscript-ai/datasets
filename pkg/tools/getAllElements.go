9bc5c71b1236f729f6dcfc1bb60eec9cd14132d2
Grant Linville <grant@acorn.io>
2024-10-14 11:21:42 -0400

    enhance: add context tool and getAllElements tool (#2)
    Signed-off-by: Grant Linville <grant@acorn.io>

---
+++ pkg/tools/getAllElements.go
@@ -0,0 +1,52 @@
+package tools
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/gptscript-ai/datasets/pkg/dataset"
+)
+
+func GetAllElements(workspace, datasetID string) {
+	m, err := dataset.NewManager(workspace)
+	if err != nil {
+		fmt.Printf("failed to create dataset manager: %v\n", err)
+		os.Exit(1)
+	}
+
+	d, err := m.GetDataset(datasetID)
+	if err != nil {
+		fmt.Printf("failed to get dataset: %v\n", err)
+		os.Exit(1)
+	}
+
+	elements := d.ListElements()
+	sort.Slice(elements, func(i, j int) bool {
+		return elements[i].Name < elements[j].Name
+	})
+
+	var elems []elem
+	for _, e := range elements {
+		eBytes, _, err := d.GetElement(e.Name)
+		if err != nil {
+			fmt.Printf("failed to get element: %v\n", err)
+			os.Exit(1)
+		}
+
+		elems = append(elems, elem{
+			Contents:    string(eBytes),
+			Name:        e.Name,
+			Description: e.Description,
+		})
+	}
+
+	elemsJSON, err := json.Marshal(elems)
+	if err != nil {
+		fmt.Printf("failed to marshal elements: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(elemsJSON))
+}
